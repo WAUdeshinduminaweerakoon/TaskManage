4024d5d29728418c2d61d53b5a62be830d82691a
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2024-04-14 14:35:26 +0530

    create the UserResponseDTO.java

---
+++ backend/TaskManager/src/main/java/com/example/TaskManager/dto/response/UserResponseDTO.java
@@ -0,0 +1,13 @@
+package com.example.TaskManager.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDTO {
+    private int id;
+    private String userName;
+}
